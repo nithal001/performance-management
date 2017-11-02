b90990d595b5f8e0f1072f97796a63a7d6e40877
nithalqb <nithal@qburst.com>
2017-11-02 15:38:10 +0530

    project structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeeViewComponent } from './employee-view/employee-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    HeaderComponent,
+    EmployeeViewComponent
   ],
   imports: [
     BrowserModule
